d1d95e9b7343538b7c34a251aa45479ed9beeba6
Wade <wz.lim.00@gmail.com>
2023-02-14 18:27:46 +0800

    starter Code

---
+++ contracts/contracts/ShortSelling.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity "0.8.17";
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./Interface/IPool.sol";
+
+contract AaveShortSeller {
+
+    address supplyToken;
+    address borrowToken;
+    uint8 leverageAmt
+
+    constructor(address _supplyToken, address _borrowToken, uint8 _leverageAmt) {
+        supplyToken = _supplyToken;
+        borrowToken = _borrowToken;
+        leverageAmt = _leverageAmt;
+    }
+
+}
--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,6 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+const {ethRPCNode} = require("./secrets.json");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  networks: {
+    localhost: {
+      chainId: 31337,
+      url: "http://127.0.0.1:8545",
+    },
+    hardhat: {
+      forking: {
+        url: ethRPCNode,
+        blockNumber: 16625969
+      }
+    }
+  },
   solidity: "0.8.17",
 };
